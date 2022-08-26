cfd0a3b007eb4538dbd1be88dde4324eef12307a
Jamie L Smyth <MrJamieLSmyth@gmail.com>
2022-08-26 06:28:23 +0000

    Add hypothesis

--- src/test/python/net/mobiustesseract/truetest/true_test_spec.py
+++ src/test/python/net/mobiustesseract/truetest/true_test_spec.py
@@ -1,14 +1,19 @@
-from nimoy.specification import Specification
-from net.mobiustesseract.truetest import TrueTest
-from random import randint, choices
+from hypothesis import given, settings
+from hypothesis.strategies import text
 from itertools import permutations
+from nimoy.specification import Specification
+from random import randint, choices
 import string
+
+from net.mobiustesseract.truetest import TrueTest
+
 
 class TrueTestSpec(Specification):
     def __init__(self, *args):
         super().__init__(*args)
         self.true_test = TrueTest()
 
+    # spec example using a data table
     def should_negate_parameter(self, parameter):
         with when:
             result = self.true_test.negate(parameter)
@@ -19,13 +24,22 @@ class TrueTestSpec(Specification):
             True        |   False
             False       |   True
 
+    # spec example using list of random properies 
     def should_reverse_parameter(self, parameter):
-
+        
         with expect:
             self.true_test.reverse(parameter) == parameter[::-1]
 
         with where:
-            parameter = [str(''.join(choices(string.ascii_letters, k = randint(1, 100))))]
+                parameter = [''.join(choices(string.ascii_letters, k=randint(1, 100))) for i in range(10)]
+
+    # spec example using hypothesis
+    @given(parameter=text(alphabet=string.ascii_letters,min_size=1,max_size=100))
+    @settings(max_examples=10)
+    def should_reverse_parameter_hypothesis_style(self, parameter):
+        
+        with expect:
+            self.true_test.reverse(parameter) == parameter[::-1]
 
     def should_and_parameters(self, parameters):
 
@@ -35,8 +49,7 @@ class TrueTestSpec(Specification):
         with where:
             parameters = [[True] * randint(2, 10)]
 
-
-
+    # spec example with permutations
     def should_not_and_parameters(self, parameters):
 
         with expect:
